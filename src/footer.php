0130007a31d8091098a271f9f87aaa8463a6a5e1
aaronperkel <aaron.perkel27@gmail.com>
2025-03-17 09:21:06 -0400

    version number

--- src/footer.php
+++ src/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>81 Buell Utilities. Updated December 2024. Version 2.1</p>
+    <p>81 Buell Utilities. Updated March 2025. Version 2.5</p>
     <p><a href="tel:4782628935">478-262-8935</a> | <a href="mailto:me@aaronperkel.com">me@aaronperkel.com</a></p>
 </footer>
 </body>
