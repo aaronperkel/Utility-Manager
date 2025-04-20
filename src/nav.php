b5fdcc3b873a5a30704a7ba7dc0602c46fbae606
aaronperkel <aaron.perkel27@gmail.com>
2025-04-20 16:44:22 -0400

    mobile tweaks

--- src/nav.php
+++ src/nav.php
@@ -5,11 +5,18 @@
     </header>
     <nav>
         <?php
-        if ($_SERVER['REMOTE_USER'] == 'aperkel') {
-            echo '<a href="index.php">Home</a>';
-            echo '<a href="portal.php">Admin Portal</a>';
-            echo '<a href="send_custom_email.php">Send Email</a>';
-        }
-        ?>
+        if ($_SERVER['REMOTE_USER'] == 'aperkel') { ?>
+            <a class="<?php if ($pathParts['filename'] == 'index') {
+                print 'activePage';
+            } ?>" href="./">Home</a>
+
+            <a class="<?php if ($pathParts['filename'] == 'portal') {
+                print 'activePage';
+            } ?>" href="./portal.php">Admin Portal</a>
+
+            <a class="<?php if ($pathParts['filename'] == 'send_custom_email') {
+                print 'activePage';
+            } ?>" href="./send_custom_email.php">Send Email</a>
+        <?php } ?>
     </nav>
 </div>
