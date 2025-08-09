2d1c42723532d9d8048c784bc0ff002c6e03d2b6
aaronperkel <me@aaronperkel.com>
2025-08-09 11:29:54 -0400

    changed theme color

--- src/top.php
+++ src/top.php
@@ -26,12 +26,14 @@ include 'connect-DB.php';
     <meta name="viewport" content="width=device-width,
         initial-scale=1.0">
 
-    <link rel="apple-touch-icon" href="/public/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/public/apple-touch-icon.png" />
     <link rel="icon" type="image/svg+xml" href="/public/favicon.svg" />
-    <link rel="icon" type="image/png" href="/public/favicon-96x96.png" />
+    <link rel="icon" type="image/png" href="/public/favicon-96x96.png" sizes="96x96" />
     <link rel="mask-icon" href="/public/favicon.svg" color="#000000" />
     <link rel="manifest" href="/public/site.webmanifest" />
-    <meta name="theme-color" content="#ffffff" />
+    <link rel="shortcut icon" href="/public/favicon.ico" />
+
+    <meta name="theme-color" content="#1a202c" />
     <meta name="apple-mobile-web-app-title" content="81 Buell" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-status-bar-style" content="default" />
